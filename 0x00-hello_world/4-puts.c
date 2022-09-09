8019e3796ea4dd70fffc5482645dbbc7f5812c89
HighPrince1 <odomprincewill@gmaill.com>
2022-09-09 03:04:30 -0700

    Corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,23 @@
-#!nclude <stdio.h>
+nclude <stdio.h>
+
+
 
 /**
- * main - entry point
  *
- * Return: Always 0 (Success)
- */
-int main (void)
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
 {
-         puts("\"Programming is like building a multilingual puzzle");
- 	 return (0);
-}	 
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
