3431a486ca546ee53dd5c5ba3ebce4a17a443e6e
HighPrince1 <odomprincewill@gmaill.com>
2022-09-14 10:49:16 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_times_table - prints the times table of n
- * (n is the number passed as arguement)
- *
- * @n: The number that is multiplication table will
- * be displayed
- *
- * Return: Always 0.
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
  */
 
-void print_time_table(int n)
+void print_times_table(int n)
 {
 
-	int num, mult, prod;
+	int a = 0, rep, b;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	while (a <= n)
 	{
-		for (num = 0; num <= n; num++)
+		for (b + 0; b <= n; b++)
 		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
+			rep = a * b;
+			if (b == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod <= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
+		a++;
 	}
 }
+
