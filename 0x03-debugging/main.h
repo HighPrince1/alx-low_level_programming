7279d18480a8abce819207e2a61ec464fda3e4b7
HighPrince1 <odomprincewill@gmaill.com>
2022-09-16 06:53:24 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -10,4 +10,8 @@ int largest_number(int a, int b, int c);
 
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
+int convert_day(int month, int day);
+
+void print_times_table(int n);
+
+#endif 
