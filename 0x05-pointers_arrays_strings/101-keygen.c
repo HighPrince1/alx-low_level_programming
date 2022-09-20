5d9c57bbaf863c5f8f8e675b8a06820f28fb1df1
HighPrince1 <odomprincewill@gmaill.com>
2022-09-20 10:47:34 -0700

    101-keygen.c101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,65 +1,35 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
- * main - entry point
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
  *
- * key_gen - password generator
- *
- * return: Always zero(success)
- *
+ * Return: Always 0 (Success)
  */
-
-int key_gen(void)
-{
-	int i;
-	int r;
-
-	char numbers[] = "0123456789";
-	char letter[] = "abcdefghijklmnoqprstuvwyzx";
-	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-	char symbols[] = "!@#$^&*?";
-	char password[12];
-
-	srand((unsigned int)(time(NULL)));
-
-	r = rand() % 4;
-
-	for (i = 0; i < 12; i++)
-	{
-		if (r == 1)
-		{
-			password[i] = numbers[rand() % 10];
-			r = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (r == 2)
-		{
-			password[i] = symbols[rand() % 8];
-			r = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (r == 3)
-		{
-			password[i] = LETTER[rand() % 26];
-			r = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else
-		{
-			password[i] = letter[rand() % 26];
-			r = rand() % 4;
-			printf("%c", password[i]);
-		}
-	}
-	return (0);
-}
-
 int main(void)
 {
-	key_gen();
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
 	return (0);
 }
